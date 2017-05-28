c0b7b833e4e8844455cd97b85b337d33a3f42f17
vs6063 <victor21898@hotmail.com>
2017-05-28 14:42:47 +1000

    re-added adv search wtf happened to it before

--- project/controllers/foodr.py
+++ project/controllers/foodr.py
@@ -40,6 +40,7 @@ def search():
     query = request.args.get('q')
     # linear search: append restaruants that contains the query
     results = []
+    #results.append()
     count = 0
     if query != "":
         querys = query.split(" ")
@@ -70,6 +71,38 @@ def search():
 
 
     return render_template('foodr/search.html', query=query, results=results, count=count)
+
+@app.route('/advsearch')
+def advSearch():
+    queryCuisine = request.args.get('c')
+    queryVicinity = request.args.get('v')
+    queryRating = request.args.get('r')
+    queryType = request.args.get('t')
+    queryAlcohol = request.args.get('a')
+    queryWheelchair = request.args.get('wh')
+    queryWifi = request.args.get("wi")
+
+    results = []
+    results.append([])
+    for r in Restaurants:
+        count = -1
+        if (queryCuisine in r.cuisine):
+            count = count + 1
+        if (queryVicinity > r.vicinity):
+            count = count + 1
+        if (queryRating < r.rating):
+            count = count + 1
+        if (queryType == r.type):
+            count = count + 1
+        if (queryAlcohol == r.alcohol):
+            count = count + 1
+        if (queryWheelchair == r.wheelchair):
+            count = count + 1
+        if (queryWifi == r.wifi):
+            count = count + 1
+        if(count > -1):
+            results[count].append(r)
+    return render_template('foodr/advsearch.html')
 
 Liked = []
 
